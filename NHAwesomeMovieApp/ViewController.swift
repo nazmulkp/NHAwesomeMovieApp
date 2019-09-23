34871ebe05194071c24c87c845cc33806f38350a
Nazmul Hasan <nazmulcsharp@gmail.com>
2019-09-23 22:59:28 +0600

    Initial Commit

---
+++ NHAwesomeMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NHAwesomeMovieApp
+//
+//  Created by Nazmul on 23/9/19.
+//  Copyright © 2019 Nazmul Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
